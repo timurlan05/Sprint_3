4535560cdaebd013d7a85f97e1231b065ae26f52
Timurlan <126976626+timurlan05@users.noreply.github.com>
2023-07-15 17:39:16 +0300

    исправление

--- test/pages.py
+++ test/pages.py
@@ -10,24 +10,24 @@ class LoginPage:
         self.home_page = f"{home_page}"
         self.button_locator = (By.CLASS_NAME, "button_button__33qZ0")
         self.link_locator = (By.CLASS_NAME, "Auth_link__1fOlj")
-        self.name_input_locator = (By.XPATH, ".//*[@id='root']/div/main/div/form/fieldset[1]/div/div/input")
+        self.name_input_locator = (By.XPATH, ".//fieldset[1]/div/div/input")
         self.email_input_locator = (By.XPATH, "//label[text()='Email']/following-sibling::input")
         self.password_input_locator = (By.CSS_SELECTOR, 'input.text.input__textfield.text_type_main-default[type="password"][name="Пароль"]')
-        self.register_button_locator = (By.CSS_SELECTOR, 'button.button_button__33qZ0.button_button_type_primary__1O7Bx.button_button_size_medium__3zxIa')
+        self.register_button_locator = (By.XPATH, './/*[@id="root"]/div/main/div/form/button')
         self.button_lk_locator = (By.CSS_SELECTOR, 'a.AppHeader_header__link__3D_hX[href="/account"]')
         self.constructor_button_locator = (By.CSS_SELECTOR, "p.AppHeader_header__linkText__3q_va.ml-2")
-        self.exit_button_locator = (By.CSS_SELECTOR, 'button.Account_button__14Yp3.text.text_type_main-medium.text_color_inactive')
+        self.exit_button_locator = (By.XPATH, './/*[@id="root"]/div/main/div/nav/ul/li[3]/button')
         self.email_lk_locator = (By.CSS_SELECTOR, 'input.text.input__textfield.text_type_main-default[name="name"]')
-        self.incorrect_password_locator = (By.XPATH, '//p[@class="input__error text_type_main-default" and text()="Некорректный пароль"]')
-        self.bulka_element_locator = (By.XPATH, ".//*[@id='root']/div/main/section[1]/div[2]/ul[1]/a[1]/p") #Флюоресцентная булка R2-D3
-        self.bulka_text_element_locator = (By.XPATH, ".//*[@id='root']/div/section[1]/div[1]/div/p")#Название в форме Флюоресцентная булка R2-D3
+        self.incorrect_password_locator = (By.XPATH, './/div/form/fieldset[3]/div/p')
+        self.bulka_element_locator = (By.XPATH, ".//section[1]/div[2]/ul[1]/a[1]/p") #Флюоресцентная булка R2-D3
+        self.bulka_text_element_locator = (By.XPATH, ".//section[1]/div[1]/div/p")#Название в форме Флюоресцентная булка R2-D3
         self.modal_element_locator = (By.CLASS_NAME, "Modal_modal__contentBox__sCy8X")
-        self.sauce_tab_element_locator = (By.XPATH, ".//*[@id='root']/div/main/section[1]/div[1]/div[2]/span") # раздел соусы
-        self.spicy_x_sauce_element_locator = (By.XPATH, ".//*[@id='root']/div/main/section[1]/div[2]/ul[2]/a[1]") # Соус Spicy-X
-        self.spicy_x_sauce_text_element_locator = (By.XPATH, ".//*[@id='root']/div/section[1]/div[1]/div/p") # название в форме Соус Spicy-X
-        self.fillings_tab_element_locator = (By.XPATH, ".//*[@id='root']/div/main/section[1]/div[1]/div[3]/span")#раздел Начинки
-        self.meat_fillings_element_locator = (By.XPATH, ".//*[@id='root']/div/main/section[1]/div[2]/ul[3]/a[1]/img")#мясо бессмертных малюсков. бессметритие не помогло
-        self.meat_fillings_text_element_locator = (By.XPATH, ".//*[@id='root']/div/section[1]/div[1]/div/p")#текст форме "Мясо бессмертных моллюсков Protostomia"
+        self.sauce_tab_element_locator = (By.XPATH, ".//section[1]/div[1]/div[2]/span") # раздел соусы
+        self.spicy_x_sauce_element_locator = (By.XPATH, ".//section[1]/div[2]/ul[2]/a[1]") # Соус Spicy-X
+        self.spicy_x_sauce_text_element_locator = (By.XPATH, ".//section[1]/div[1]/div/p") # название в форме Соус Spicy-X
+        self.fillings_tab_element_locator = (By.XPATH, ".//section[1]/div[1]/div[3]/span")#раздел Начинки
+        self.meat_fillings_element_locator = (By.XPATH, ".//section[1]/div[2]/ul[3]/a[1]/img")#мясо бессмертных малюсков. бессметритие не помогло
+        self.meat_fillings_text_element_locator = (By.XPATH, ".//section[1]/div[1]/div/p")#текст форме "Мясо бессмертных моллюсков Protostomia"
 
     def click_button(self):
         button = self.driver.find_element(*self.button_locator)
